936ac7150e4510564e54fcc6597795e5532b563d
Amy Huang <ahuang@ucvts.org>
2020-03-10 23:24:09 -0400

    18

--- src/pak/Dictionary.java
+++ src/pak/Dictionary.java
@@ -1,10 +1,46 @@
 package pak;
+import pak.*;
 
 import java.awt.EventQueue;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultCaret;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.BorderFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.CardLayout;
+import java.awt.Font;
 
 public class Dictionary {
 
@@ -20,19 +56,34 @@ public class Dictionary {
 
 	/**
 	 * Launch the application.
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		getWords();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Dictionary window = new Dictionary();
-					window.frame.setVisible(true);
+					window.frmDictionary.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+	private static DefaultListModel<String> getWords() throws FileNotFoundException{
+	    Gson gson = new Gson();
+	        String classpathDirectory = Utils.getClasspathDir();
+	        BufferedReader br = new BufferedReader(new FileReader(classpathDirectory + "words.json"));
+	        Word[] words = gson.fromJson(br, Word[].class);
+	        System.out.println(words.length + " words added");
+	        DefaultListModel<String> listOfWords = new DefaultListModel<String>();
+	        for (Word word : words) {
+	          listOfWords.addElement(word.getWord());
+	        }
+	       ;
+	        return  Utils.sortWordsAsc(listOfWords);
+	  }
 
 	/**
 	 * Create the application.
@@ -45,7 +96,9 @@ public class Dictionary {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		JFrame frame = new JFrame();
+		frame.setTitle("Dictionary");
+		frame = frame;
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
