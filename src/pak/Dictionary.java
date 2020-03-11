981948a9a64a78379411b0a428aeb09bed6e5cf7
Amy Huang <ahuang@ucvts.org>
2020-03-10 22:38:55 -0400

    17

--- src/pak/Dictionary.java
+++ src/pak/Dictionary.java
@@ -2,11 +2,21 @@ package pak;
 
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Dictionary {
 
-	private JFrame frame;
+	  private JFrame frmDictionary;
+	  private JTextField txtSearch;
+	  private final ButtonGroup buttonGroup = new ButtonGroup();
+	  private JTextField textField;
+	  private JTextField txtDefinitions;
+	  private JTextField textField_2;
+	  private JTextField textField_1;
+	  private JTextField textField_3;
+
 
 	/**
 	 * Launch the application.
